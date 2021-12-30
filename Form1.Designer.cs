b1a7fdc9a122143a897f8419eb6423c25c5d7964
johanericsson <johan@ericssonfamily.com>
2021-12-30 13:54:53 -0800

    Initial checkin

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Renamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pathEdt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.prefixEdt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pathEdt
+            // 
+            this.pathEdt.Location = new System.Drawing.Point(90, 19);
+            this.pathEdt.Name = "pathEdt";
+            this.pathEdt.Size = new System.Drawing.Size(308, 20);
+            this.pathEdt.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Path";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Prefix";
+            // 
+            // prefixEdt
+            // 
+            this.prefixEdt.Location = new System.Drawing.Point(90, 56);
+            this.prefixEdt.Name = "prefixEdt";
+            this.prefixEdt.Size = new System.Drawing.Size(100, 20);
+            this.prefixEdt.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Go!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.resultLabel);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.pathEdt);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.prefixEdt);
+            this.groupBox1.Location = new System.Drawing.Point(25, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(467, 125);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Renumber";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(192, 87);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(154, 13);
+            this.resultLabel.TabIndex = 7;
+            this.resultLabel.Text = "ZZZZZZZZZZZZZZZZZZZZZ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(404, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(48, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 149);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Renamer";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox pathEdt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox prefixEdt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Drawing.Imaging;
+namespace Renamer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            resultLabel.Text = "";
+        }
+
+        private static Regex r = new Regex(":");
+
+        //retrieves the datetime WITHOUT loading the whole image
+        public static DateTime GetDateTakenFromImage(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            using (Image myImage = Image.FromStream(fs, false, false))
+            {
+                PropertyItem propItem = myImage.GetPropertyItem(36867);
+                string dateTaken = r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+                return DateTime.Parse(dateTaken);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    pathEdt.Text = fbd.SelectedPath;
+                    string[] files = Directory.GetFiles(pathEdt.Text);
+                    resultLabel.Text = "Found " + files.Length + " files.";
+
+                }
+            }
+        }
+
+      /*  class DateTimeCompare : IComparer<string>
+        {
+            public override int IComparer.Compare(string o1,string o2)
+            {
+                return 0;
+            }
+        }*/
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] files = Directory.GetFiles(pathEdt.Text);
+                Array.Sort(files, (emp1, emp2) => GetDateTakenFromImage(emp1).CompareTo(GetDateTakenFromImage(emp2)));
+                string p = prefixEdt.Text;
+                if (p.Length == 0)
+                    throw new Exception("Prefix not defined");
+                
+                for (int i = 0; i < files.Length; i++)
+                {
+                    // extract extension
+                    string filename = files[i];
+                    string extension = Path.GetExtension(filename);
+                    string baseName = Path.GetFileNameWithoutExtension(filename);
+                    string baseDirectory = Path.GetDirectoryName(filename);
+                    string target = baseDirectory + "\\" + p + "-" + (i + 1).ToString()  + extension;
+                    File.Move(files[i], target);
+                }
+                resultLabel.Text = "Successfully renamed " + files.Length + " files.";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
